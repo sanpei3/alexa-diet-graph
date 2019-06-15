87075dcc0917559053f44a468a7e4728486864ba
Yoshiro MIHIRA <sanpei@sanpei.org>
2019-06-15 21:04:03 +0900

    calculate how much different previous measure

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ process.env.TZ = "Asia/Tokyo";
 // please define "Environment variables " field at AWS lambda console
 const APP_ID = process.env.ALEXA_APP_ID;
 
+function isNoon(date) {
+    var hour = new Date(date).getHours().toString();
+    if (hour >= 5 && hour < 15) {
+	return true;
+    } else {
+	return false;
+    }
+}
+
 function help(t) {
     const helpmsg = [
 	"体重グラフスキルを使うと、Webサービス体重グラフに体重を記録することができます。ご利用の際は、「30kg記録してくれる」のように話しかけてください。終了方法は、終了と呼びかけてください。体重をキログラムで答えてください。",
@@ -39,6 +48,7 @@ function updateDiet(weight, accessToken, self) {
     var month = (new Date(date).getMonth() +1).toString();
     var day = new Date(date).getDate().toString();
     var hour = new Date(date).getHours().toString();
+    var server_error_message = '記録に失敗しました。体重グラフのサーバが不調な可能性があります時間を置いてから試みてください';
     
     var options = {
         method: 'POST',
@@ -58,29 +68,87 @@ function updateDiet(weight, accessToken, self) {
         },
     };
     
-    rp(options).then((response) => {
-	if (response.match(/<p>ログアウトまたはタイムアウトしました。<\/p>/)) {
-	    self.attributes['serverError'] = 0;
-	    self.emit(':tellWithLinkAccountCard','アカウントリンクの有効期限が切れているようです。アカウントリンクを再設定してください');
-	} else if (response.match(/登録しました。<br>/)) {
-	    self.attributes['serverError'] = 0;
-	    self.emit(':tell', weight + 'kg で記録しました。');
-	} else {
-	    self.attributes['serverError'] = 0;
-	    self.emit(':tell', '記録に失敗しました。体重グラフのサーバが不調な可能性があります時間を置いてから試みてください');
+    var options_get_prev_weight = {
+        method: 'GET',
+        uri: "http://diet.dyndns.org/?cmd=weight_prev&count=10",
+        headers: {
+	    'Authorization': "Bearer " + accessToken,
+        },
+    };
+
+    return rp(options_get_prev_weight).then((response) => {
+	var res = JSON.parse(response);
+
+	var date = Date.now();
+	var noonFlag = isNoon(date);
+	var prevWeight = 0;
+	var prevDiff = 0;
+	var prevDays = 0;
+	var prevHours = 0;
+	var oneDay = 60*60*24;
+	var diffMessage = "";
+
+	res.data.some(function(val, index) {
+	    if (isNoon(val.timestamp*1000) == noonFlag) {
+		prevDiff = val.diff;
+		prevDays = parseInt(prevDiff / oneDay);
+		prevHours = parseInt((prevDiff % oneDay) / 60 / 60);
+
+		if (!(prevDays == 0 && prevHours == 0)) {
+		    prevWeight = val.weight;
+		    return true;
+		}
+	    }
+	});
+	if (prevWeight != 0) {
+	    var diffWeight = Math.round((weight - prevWeight)*10) * 100;
+	    if (Math.abs(diffWeight) >= 10*1000) {
+		const message = weight +"kgと前回から10kg以上の変化があります、もう一度、体重を教えてください。";
+		self.emit(':ask', message);
+		return;
+	    }
+	    if (prevDays > 0) {
+		var diffMessage = prevDays +"日前から"
+	    } else {
+		var diffMessage = prevHours+ "時間前から"
+	    }
+	    if (diffWeight != 0) {
+		if (diffWeight > 0) {
+		    diffMessage = diffMessage + diffWeight +"g増えました。";
+		} else {
+		    diffMessage = diffMessage + diffWeight * (-1) +"g減りました。";
+		}
+	    } else {
+		diffMessage = diffMessage + "変化はありませんでした。";
+	    }
 	}
+	rp(options).then((response) => {
+	    if (response.match(/<p>ログアウトまたはタイムアウトしました。<\/p>/)) {
+		self.attributes['serverError'] = 0;
+		self.emit(':tellWithLinkAccountCard','アカウントリンクの有効期限が切れているようです。アカウントリンクを再設定してください');
+	    } else if (response.match(/登録しました。<br>/)) {
+		self.attributes['serverError'] = 0;
+		self.emit(':tell', weight + 'kg で記録しました。' + diffMessage);
+	    } else {
+		self.attributes['serverError'] = 0;
+		self.emit(':tell', server_error_message);
+	    }
+	}, (error) => {
+	    var serverError = Number(t.attributes['serverError']);
+	    if (isNaN(serverError) || serverError == 0) {
+		self.attributes['serverError'] = 1;
+		self.emit(':ask', '記録に失敗しました。再度体重を教えてください。');
+	    } else {
+		self.attributes['serverError'] = 0;
+		self.emit(':tell', server_error_message);
+	    }
+	});
     }, (error) => {
-	var serverError = Number(t.attributes['serverError']);
-	if (isNaN(serverError) || serverError == 0) {
-	    self.attributes['serverError'] = 1;
-	    self.emit(':ask', '記録に失敗しました。再度体重を教えてください。');
-	} else {
-	    self.attributes['serverError'] = 0;
-	    self.emit(':tell', '記録に失敗しました。体重グラフのサーバが不調な可能性があります時間を置いてから試みてください');
-	}
+	self.attributes['serverError'] = 0;
+	self.emit(':tell', server_error_message);
     });
 }
- 
+
 function convertDotNumberStringToDotNumber(s, maxNumberOfDigit) {
     let dotWeight = Number(s);
     if (!isNaN(dotWeight)) {
